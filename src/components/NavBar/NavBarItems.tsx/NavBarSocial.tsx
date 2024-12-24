31d4bdee821d0d150cd4a6e76309c17cde5dd7b1
Sasa Nedic <svetasmak0@gmail.com>
2024-12-24 22:29:18 +0100

    Create style for NavBarSocial

--- src/components/NavBar/NavBarItems.tsx/NavBarSocial.tsx
+++ src/components/NavBar/NavBarItems.tsx/NavBarSocial.tsx
@@ -3,9 +3,14 @@ import Icon from "../../../UX/Icons";
 const NavBarSocial = () => {
   return (
     <div className="navBarSocial">
-      <Icon name="facebook" color="white" size="20" />
-      <Icon name="instagram" color="white" size="20" />
-      <Icon name="tiktok" color="white" size="20" />
+      <Icon name="facebook" color="white" size="24" className="icon-facebook" />
+      <Icon
+        name="instagram"
+        color="white"
+        size="24"
+        className="icon-instagram"
+      />
+      <Icon name="tiktok" color="white" size="24" className="icon-tiktok" />
     </div>
   );
 };
