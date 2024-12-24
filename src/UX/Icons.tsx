66053043ed55daed20ab3ef9a3999eaaf73aa378
Sasa Nedic <svetasmak0@gmail.com>
2024-12-24 03:04:00 +0100

    Create reusable Icons component

--- src/UX/Icons.tsx
+++ src/UX/Icons.tsx
@@ -1,4 +1,36 @@
-// const Icons = ({ type }) => {
-//   return <p type={type}></p>;
-// };
-// export default Icons;
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+
+interface IconProps {
+  name: string;
+  size?: number;
+  color?: string;
+  onClick?: () => void;
+  className?: string;
+}
+
+const Icon = ({
+  name,
+  size = 24,
+  color = "black",
+  onClick = () => {},
+  className = "",
+}: IconProps) => {
+  const icons: { [key: string]: React.ElementType } = {
+    facebook: FaFacebook,
+    instagram: FaInstagram,
+    tiktok: FaTiktok,
+  };
+
+  const SelectedIcon = icons[name];
+
+  return (
+    <SelectedIcon
+      size={size}
+      color={color}
+      onClick={onClick}
+      className={className}
+    />
+  );
+};
+
+export default Icon;
