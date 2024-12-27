6c1c4194e8778f700d9d9b62a354e71a0843a8a2
Sasa Nedic <svetasmak0@gmail.com>
2024-12-27 19:59:32 +0100

    Add useRef in Products and display DropMenu

--- src/components/Menu/MenuItems/Products.tsx
+++ src/components/Menu/MenuItems/Products.tsx
@@ -1,16 +1,45 @@
+import { useRef, useState } from "react";
 import DropMenu from "../../../UX/DropMenu";
 import Icon from "../../../UX/Icons";
 
 const Products = () => {
+  const productRef = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    if (productRef.current) {
+      productRef.current.style.backgroundColor = "red";
+      productRef.current.style.backgroundColor = "red";
+      console.log("na btn", productRef.current);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    if (productRef.current) {
+      productRef.current.style.backgroundColor = "white";
+      console.log("skinuo se sa btn", productRef.current);
+    }
+  };
+
+  console.log("productRef.current", productRef);
+
   return (
-    <div className="products">
+    <div
+      className="products"
+      ref={productRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <span>
-        <Icon name="burger" size="26" color="#e32726" />
-        <p>Products</p>
+        <Icon name="burger" size="26" color={isHovered ? "white" : "#e32726"} />
+        <p className={isHovered ? "white" : "red"}>Products</p>
       </span>
-      <Icon name="down" color="#e32726" />
-      <DropMenu />
+      <Icon name="down" color={isHovered ? "white" : "#e32726"} />
+      {isHovered && <DropMenu />}
     </div>
   );
 };
+
 export default Products;
