5bbc4a6b10d11fcc204d854bfbfdb78b0423a180
Sasa Nedic <svetasmak0@gmail.com>
2024-12-30 12:30:44 +0100

    Working on firstSlider

--- src/layout/Slider/Slider.tsx
+++ src/layout/Slider/Slider.tsx
@@ -1,5 +1,11 @@
+import FirstSliderItem from "./SlidersItems/FirstSliderItem";
+
 const Slider = () => {
-  return <div></div>;
+  return (
+    <div className="slider">
+      <FirstSliderItem />
+    </div>
+  );
 };
 
 export default Slider;
--- src/pages/Home/HomeHelpers.tsx/Home.tsx
+++ src/pages/Home/HomeHelpers.tsx/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "../../../components/Header/Header";
 import Menu from "../../../components/Menu/Menu";
 import NavBar from "../../../components/NavBar/NavBar";
+import Slider from "../../../layout/Slider/Slider";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <NavBar />
       <Header />
       <Menu />
+      <Slider />
     </div>
   );
 };
