b0566c293fca63044bd85178b7f6a92a50b952fa
Sasa Nedic <svetasmak0@gmail.com>
2025-01-01 03:52:16 +0100

    Create TopDeals component

--- src/pages/Home/HomeHelpers.tsx/Home.tsx
+++ src/pages/Home/HomeHelpers.tsx/Home.tsx
@@ -3,6 +3,7 @@ import Menu from "../../../components/Menu/Menu";
 import NavBar from "../../../components/NavBar/NavBar";
 import BestSellers from "../../../layout/BestSellers/BestSellers";
 import Slider from "../../../layout/Slider/Slider";
+import TopDeals from "../../../layout/TopDeals/TopDeals";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Menu />
       <Slider />
       <BestSellers />
+      <TopDeals />
     </div>
   );
 };
