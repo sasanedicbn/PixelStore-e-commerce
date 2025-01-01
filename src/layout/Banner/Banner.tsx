b6140f8d5f32860f9e9adbd614cddacfbe1fdde2
Sasa Nedic <svetasmak0@gmail.com>
2025-01-01 21:53:41 +0100

    Create Banner

---
+++ src/layout/Banner/Banner.tsx
@@ -0,0 +1,10 @@
+import BannerLaptops from "./BannerItems/BannerLapTops";
+
+const Banner = () => {
+  return (
+    <div className="banner-container">
+      <BannerLaptops />
+    </div>
+  );
+};
+export default Banner;
---
+++ src/layout/Banner/BannerItems/BannerLaptops.tsx
@@ -0,0 +1,12 @@
+const BannerLaptops = () => {
+  return (
+    <div>
+      <img
+        src="../middle-banner/middle-laptop-banner-2.jpg"
+        alt="Laptops banner"
+      />
+    </div>
+  );
+};
+
+export default BannerLaptops;
