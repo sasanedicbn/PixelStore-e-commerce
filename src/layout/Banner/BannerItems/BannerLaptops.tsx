2932463cf055c965234050d9f7b141f2289e8d8d
Sasa Nedic <svetasmak0@gmail.com>
2025-01-02 02:21:44 +0100

    Create style for bannerlaptops

--- src/layout/Banner/BannerItems/BannerLaptops.tsx
+++ src/layout/Banner/BannerItems/BannerLaptops.tsx
@@ -5,6 +5,12 @@ const BannerLaptops = () => {
         src="../public/assets/middle-banner/middle-laptop-banner-2.jpg"
         alt="Laptops banner"
       />
+      <div className="banner-laptops-description">
+        <p className="new-arrivals">New Arrivals</p>
+        <h1>Select Laptops</h1>
+        <p className="save-up">SAVE UP TO</p>
+      </div>
+      <span>$100!</span>
     </div>
   );
 };
