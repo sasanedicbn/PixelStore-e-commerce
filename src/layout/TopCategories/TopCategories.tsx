729223b571a4dd67ca8d5571e9ad585308b46dce
Sasa Nedic <svetasmak0@gmail.com>
2025-01-02 12:13:09 +0100

    Create changes in TopCategories component

--- src/layout/TopCategories/TopCategories.tsx
+++ src/layout/TopCategories/TopCategories.tsx
@@ -1,5 +1,37 @@
+import SingleCategory from "./TopCategoriesItems/SingleCategory";
+
 const TopCategories = () => {
-  return <div></div>;
+  const topCategory = [
+    {
+      name: "TVs",
+      img: "../public/assets/top-categories/TVsnew.jpg",
+    },
+    {
+      name: "Computers",
+      img: "../public/assets/top-categories/Computersnew.jpg",
+    },
+    {
+      name: "Gaming",
+      img: "../public/assets/top-categories/Gamingnew.jpg",
+    },
+    {
+      name: "Audio",
+      img: "../public/assets/top-categories/Audionew.webp",
+    },
+    {
+      name: "Appliances",
+      img: "../public/assets/top-categories/Photograpy.jpg",
+    },
+  ];
+
+  return (
+    <div>
+      <h1>Shop Top Categories</h1>
+      {topCategory.map((category, index) => (
+        <SingleCategory key={index} name={category.name} img={category.img} />
+      ))}
+    </div>
+  );
 };
 
 export default TopCategories;
