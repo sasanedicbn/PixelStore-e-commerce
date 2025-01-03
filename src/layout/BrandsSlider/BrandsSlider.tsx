0e52d034b66bf3933d65b83eeb480b072540bb72
Sasa Nedic <svetasmak0@gmail.com>
2025-01-03 13:42:59 +0100

    Create changes

---
+++ src/layout/BrandsSlider/BrandsSlider.tsx
@@ -0,0 +1,10 @@
+const BrandsSlider = () => {
+  return (
+    <div className="brands-slider-container">
+      <h2 className="brands-slider-title">Brands We Love</h2>
+      <div className="brands-slider"></div>
+    </div>
+  );
+};
+
+export default BrandsSlider;
