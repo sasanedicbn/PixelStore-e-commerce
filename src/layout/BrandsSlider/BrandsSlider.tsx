e338431c18fb5a5518c8dfdc3d82f0449956a0b0
Sasa Nedic <svetasmak0@gmail.com>
2025-01-03 13:53:37 +0100

    Create brands

--- src/layout/BrandsSlider/BrandsSlider.tsx
+++ src/layout/BrandsSlider/BrandsSlider.tsx
@@ -1,8 +1,18 @@
+import BrandsSliderItem from "./BrandsSliderItem/BrandsSliderItem";
+
 const BrandsSlider = () => {
+  const brands = [
+    { src: "../public/assets/brands/Acer.jpg", alt: "Acer brand" },
+    { src: "../public/assets/brands/Asus.jpg", alt: "Asus brand" },
+    { src: "../public/assets/brands/Lenovo.jpg", alt: "Lenovo brand" },
+    { src: "../public/assets/brands/Microsoft.jpg", alt: "Microsoft brand" },
+    { src: "../public/assets/brands/Sigma.jpg", alt: "Sigma brand" },
+  ];
+
   return (
     <div className="brands-slider-container">
       <h2 className="brands-slider-title">Brands We Love</h2>
-      <div className="brands-slider"></div>
+      <BrandsSliderItem />
     </div>
   );
 };
---
+++ src/layout/BrandsSlider/BrandsSliderItem/BrandsSliderItem.tsx
@@ -0,0 +1,14 @@
+const BrandsSliderItem = () => {
+  return (
+    <div className="brands-item">
+      <div className="brands-imgs">
+        <img src="../public/assets/brands/Acer.jpg" alt="Acer brand" />
+        <img src="../public/assets/brands/Asus.jpg" alt="Acer brand" />
+        <img src="../public/assets/brands/Lenovo.jpg" alt="Acer brand" />
+        <img src="../public/assets/brands/Microsoft.jpg" alt="Acer brand" />
+        <img src="../public/assets/brands/Sigma.jpg" alt="Acer brand" />
+      </div>
+    </div>
+  );
+};
+export default BrandsSliderItem;
