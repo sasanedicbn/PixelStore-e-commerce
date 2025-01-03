59b0c2713b5f0cba282e4a2ace504b15a49d206c
Sasa Nedic <svetasmak0@gmail.com>
2025-01-03 14:44:02 +0100

    Create changes in code

--- src/layout/Services/Services.tsx
+++ src/layout/Services/Services.tsx
@@ -1,7 +1,12 @@
+import ServicesItems from "./ServicesItems/ServicesItems";
+
 const Services = () => {
+  // objekat odnosno niz ovdje napravi i mapuj ga nemoj ovo
   return (
     <div className="services-container">
-      <div className="services-items"></div>
+      <div className="services-items">
+        <ServicesItems icon="" title="FREE SHIPPING & RETURN" />
+      </div>
     </div>
   );
 };
--- src/layout/Services/ServicesItems/ServicesItems.tsx
+++ src/layout/Services/ServicesItems/ServicesItems.tsx
@@ -1,8 +1,13 @@
+import Icon from "../../../UX/Icons";
+
 const ServicesItems = ({ icon, title, description }) => {
   return (
     <div className="single-services">
-      <h4>FREE SHIPPING & RETURN</h4>
-      <p>Free shipping on all orders over $35.</p>
+      <Icon type={icon} size="24" color="red" />
+      <div className="single-descriptions">
+        <h4>FREE SHIPPING & RETURN</h4>
+        <p>Free shipping on all orders over $35.</p>
+      </div>
     </div>
   );
 };
