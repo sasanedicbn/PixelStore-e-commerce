4673841278bae1d59ab6a53f8e83cf83e1d6ebd8
Sasa Nedic <svetasmak0@gmail.com>
2025-01-04 21:36:27 +0100

    Craete changes

--- src/layout/Services/Services.tsx
+++ src/layout/Services/Services.tsx
@@ -1,6 +1,7 @@
 import ServicesItems from "./ServicesItems/ServicesItems";
 
 const Services = () => {
+  // objekat odnosno niz ovdje napravi i mapuj ga nemoj ovo
   return (
     <div className="services-container">
       <div className="services-items">
