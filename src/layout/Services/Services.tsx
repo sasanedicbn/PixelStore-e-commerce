25d43292b2d09498e763f5c6362b56e0c0928dbc
Sasa Nedic <svetasmak0@gmail.com>
2025-01-04 23:34:51 +0100

    Add in serveces array new icons

--- src/layout/Services/Services.tsx
+++ src/layout/Services/Services.tsx
@@ -4,19 +4,22 @@ const Services = () => {
   // objekat odnosno niz ovdje napravi i mapuj ga nemoj ovo
   const services = [
     {
-      icon:""
+      icon: "truck",
       title: "FREE SHIPPING & RETURN",
       description: "Free shipping for all orders over $35",
     },
     {
+      icon: "currency",
       title: "PRICE MATCH GUARANTEE",
       description: "Score the lowest price possible, period.",
     },
     {
+      icon: "services",
       title: "ONLINE SUPPORT 24/7",
       description: "We are here to help. 24/7 Support.",
     },
     {
+      icon: "payment",
       title: "SECURE PAYMENT",
       description: "Secure and easy online checkout.",
     },
