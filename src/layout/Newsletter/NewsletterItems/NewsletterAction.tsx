ddd8e9984c9cf3fec66c69a6300caceaa817bb5d
Sasa Nedic <svetasmak0@gmail.com>
2025-01-05 14:59:50 +0100

    Create NewsletterAction component

---
+++ src/layout/Newsletter/NewsletterItems/NewsletterAction.tsx
@@ -0,0 +1,16 @@
+import "./NewsletterAction.scss";
+
+const NewsletterAction = () => {
+  return (
+    <div className="newsletter-container">
+      <input
+        type="email"
+        placeholder="Email address"
+        className="newsletter-input"
+      />
+      <button className="newsletter-button">Subscribe</button>
+    </div>
+  );
+};
+
+export default NewsletterAction;
