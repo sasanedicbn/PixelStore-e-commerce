96ca02f1cebe9b12aaeb70d1156657ca8cc342e6
Sasa Nedic <svetasmak0@gmail.com>
2025-01-05 14:28:16 +0100

    Create file Newsletter

---
+++ src/layout/Newsletter/Newsletter.tsx
@@ -0,0 +1,5 @@
+const Newsletter = () => {
+  return <div className="newsletter"></div>;
+};
+
+export default Newsletter;
--- src/layout/Services/ServicesItems/ServicesItems.tsx
+++ src/layout/Services/ServicesItems/ServicesItems.tsx
@@ -3,7 +3,7 @@ import Icon from "../../../UX/Icons";
 const ServicesItems = ({ icon, title, description }) => {
   return (
     <div className="single-services">
-      <Icon name={icon} size="32" color="#e32726" />
+      <Icon name={icon} size="34" color="#e32726" className="bg-color" />
       <div className="single-descriptions">
         <h4 className="title">{title}</h4>
         <p className="description">{description}</p>
