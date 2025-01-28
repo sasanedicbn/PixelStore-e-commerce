a3022d89ab8bdb181023686089f7016c45f0b894
Sasa Nedic <svetasmak0@gmail.com>
2025-01-28 23:32:13 +0100

    Add midleware

--- server/server.js
+++ server/server.js
@@ -7,10 +7,12 @@ dotenv.config();
 const PORT = process.env.PORT || 6000;
 const MONGOURL = process.env.MONGO_URL;
 
+app.use(express.json());
+
 mongoose
   .connect(MONGOURL)
   .then(() => {
-    console.log("Database connected sucessufuly");
+    console.log("Database connected successfully");
     app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   })
   .catch((error) => {
