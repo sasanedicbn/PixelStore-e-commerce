7fb79a919faaf0fa4b5019c336366faa4dacd939
Sasa Nedic <svetasmak0@gmail.com>
2025-01-30 23:33:25 +0100

    Wokring on sendData fn

--- client/src/layout/Slider/Slider.tsx
+++ client/src/layout/Slider/Slider.tsx
@@ -17,6 +17,31 @@ const Slider = () => {
     setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
   };
 
+  const sendData = async () => {
+    const response = await fetch("./public/assets/products.json");
+    const products = await response.json();
+    console.log("products", products);
+    const payload = new FormData();
+    console.log(payload, "pejload");
+    const promises = products.map(async (product, index) => {
+      console.log("product unutra", product);
+      payload.append(`title_${index}`, product.title);
+      payload.append(`price_${index}`, product.price);
+      payload.append(`category_${index}`, product.category);
+
+      try {
+        const imageResponse = await fetch(product.image);
+        const blob = await imageResponse.blob();
+        payload.append(`image_${index}`, blob, `image_${index}.jpg`);
+      } catch (error) {
+        console.error(`Error loading image ${product.image}:`, error);
+      }
+    });
+    const da = await Promise.all(promises);
+
+    console.log("final promises", da);
+  };
+
   return (
     <div className="slider">
       <Button type="sliderBtnLeft" onClick={prevSlide}>
@@ -26,6 +51,7 @@ const Slider = () => {
       <Button type="sliderBtnRight" onClick={nextSlide}>
         <Icon name="sliderRight" size="24" color="red" />
       </Button>
+      <button onClick={() => sendData()}>SEND DATA TO BACKEND</button>
     </div>
   );
 };
