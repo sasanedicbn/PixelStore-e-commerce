f1a752d8134536e43eca994e32538237350d4de4
Sasa Nedic <svetasmak0@gmail.com>
2025-02-05 23:59:45 +0100

    Create change

--- client/src/pages/SingleProduct/SingleProductItems/ProductDetails.tsx
+++ client/src/pages/SingleProduct/SingleProductItems/ProductDetails.tsx
@@ -4,6 +4,7 @@ const ProductDetails = ({ product }) => {
     <div>
       <h2>{product.product.title}</h2>
       <p>${product.product.price}</p>
+      <p>{product.product.available}</p>
       <p>{product.product.description}</p>
     </div>
   );
