41faaeecfdd9d4d38b15be57eddff90d1c48736d
Sasa Nedic <svetasmak0@gmail.com>
2025-02-06 17:23:15 +0100

    Finish style for ProductDetails

--- client/src/pages/SingleProduct/SingleProductItems/ProductDetails.tsx
+++ client/src/pages/SingleProduct/SingleProductItems/ProductDetails.tsx
@@ -5,20 +5,29 @@ const ProductDetails = ({ product }) => {
   const staticStars = 5;
   return (
     <div className="product-details">
-      <h1>{product.product.title}</h1>
+      <h1>{product.product.title} - M1 chip (Latest model)</h1>
       <div className="static-stars">
         {[...Array(staticStars)].map((_, index) => (
           <span key={index}>&#9733;</span>
         ))}
         <p> Be the first to review this product</p>
       </div>
-      <p>${product.product.price}</p>
-      <p>{product.product.details.available}</p>
+      <p className="price">${product.product.price}</p>
+      <p className="available">
+        {" "}
+        AVAILABILITY:{" "}
+        <span>{product.product.details.available.toUpperCase()}</span>
+      </p>
+      <p className="mpn">
+        {" "}
+        MPN: <span>MGN63LL/A</span>
+      </p>
       <p>{product.product.description}</p>
-      <ReviewStars
+      {/* <ReviewStars
         totalStars={5}
         onRatingChange={(rating) => console.log(rating)}
-      />
+      /> */}
+      <p>SASA</p>
     </div>
   );
 };
