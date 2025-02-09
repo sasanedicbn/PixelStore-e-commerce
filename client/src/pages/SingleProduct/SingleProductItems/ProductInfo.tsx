e76e7c160611b53f6b9203fc18918a42c648cc84
Sasa Nedic <svetasmak0@gmail.com>
2025-02-09 10:54:08 +0100

    Create renderInfo function

--- client/src/pages/SingleProduct/SingleProductItems/ProductInfo.tsx
+++ client/src/pages/SingleProduct/SingleProductItems/ProductInfo.tsx
@@ -1,6 +1,17 @@
 import Button from "../../../UX/Button";
 
-const ProductInfo = () => {
+const ProductInfo = ({ product }) => {
+  const renderInfo = () => {
+    switch (activeTab) {
+      case "details":
+        return <p>{product.product.description || "No details available"}</p>;
+      case "moreInfo":
+        return <p>{product.product.price || "No available more info."}</p>;
+      case "reviews":
+        return <p>REVI</p>;
+    }
+  };
+
   return (
     <div className="productinfo">
       <Button type="info">Details</Button>
