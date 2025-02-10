b756c245d8db2d6f75c69c0b5537a563317e5b95
Sasa Nedic <svetasmak0@gmail.com>
2025-02-10 23:50:55 +0100

    Fill Reviews component

--- client/src/pages/SingleProduct/SingleProductItems/Reviews.tsx
+++ client/src/pages/SingleProduct/SingleProductItems/Reviews.tsx
@@ -1,5 +1,49 @@
 const Reviews = () => {
-  return <div>SASA</div>;
+  return (
+    <div className="reviews-container">
+      <h2>You're reviewing:</h2>
+      <form>
+        <div className="rating">
+          <span>Ocjena:</span>
+          {[1, 2, 3, 4, 5].map((star) => (
+            <span
+              key={star}
+              //   className={`star ${star <= rating ? "selected" : ""}`}
+            >
+              ★
+            </span>
+          ))}
+        </div>
+        <div className="form-group">
+          <label htmlFor="nickname">Nickname:</label>
+          <input
+            type="text"
+            id="nickname"
+            // value={nickname}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="summary">Summary:</label>
+          <input
+            type="text"
+            id="summary"
+            // value={summary}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="review">Review:</label>
+          <textarea
+            id="review"
+            // value={review}
+            required
+          />
+        </div>
+        <button type="submit">SUBMIT REVIEW</button>
+      </form>
+    </div>
+  );
 };
 
 export default Reviews;
