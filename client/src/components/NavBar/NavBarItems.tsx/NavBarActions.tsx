08e08f9d02af214f3dad8c43871666740fe4bdb2
Sasa Nedic <svetasmak0@gmail.com>
2025-02-16 18:13:18 +0100

    add action in NavBarActions component

--- client/src/components/NavBar/NavBarItems.tsx/NavBarActions.tsx
+++ client/src/components/NavBar/NavBarItems.tsx/NavBarActions.tsx
@@ -7,7 +7,7 @@ const NavBarActions = () => {
         <li>Call us at +38766992321</li>
         <Link to="/customer/signin">Sign in</Link>
         <Link to="contact">Contact us</Link>
-        <Link to="">Create An Account</Link>
+        <Link to="createAccount">Create An Account</Link>
       </ul>
     </div>
   );
