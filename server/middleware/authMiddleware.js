8340f9bcbb8b104c1846ae1761d74fc796a950aa
Sasa Nedic <svetasmak0@gmail.com>
2025-02-19 23:12:48 +0100

    Create protect route

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import UserModel from "../model/user.js";
+
+export const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      // Get user from token
+      req.user = await UserModel.findById(decoded.id).select("-password");
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) return res.status();
+});
