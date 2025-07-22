e2a5663d38b681ff14251c5af01a08a0420553d6
Sasa Nedic <svetasmak0@gmail.com>
2025-07-22 20:55:35 +0200

    fix component

--- client/src/components/Forms/TextareaInput.tsx
+++ client/src/components/Forms/TextareaInput.tsx
@@ -3,7 +3,9 @@ const TextareaInput = ({ label, name, register, errors }) => {
     <div className="textInput">
       <label>{label}</label>
       <textarea {...register(name)} placeholder={label} />
-      {errors[name] && <p>{error
+      {errors[name] && <p>{errors[name].message}</p>}
+    </div>
+  );
 };
 
 export default TextareaInput;
