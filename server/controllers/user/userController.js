2828197118e27d22216d56c199c6b4d14fab252a
Sasa Nedic <svetasmak0@gmail.com>
2025-07-25 20:49:30 +0200

    Create changes

--- server/controllers/user/userController.js
+++ server/controllers/user/userController.js
@@ -110,7 +110,6 @@ export const sendMessageUser = async (req, res) => {
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-      },
     });
 
     await transporter.sendMail({
