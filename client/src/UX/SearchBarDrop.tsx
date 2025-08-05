8b4cd29197017ef156bd31c814bcf8efd43bb234
Sasa Nedic <svetasmak0@gmail.com>
2025-08-05 09:13:37 +0200

    Try implement fetch by single product in SearchBar

--- client/src/UX/SearchBarDrop.tsx
+++ client/src/UX/SearchBarDrop.tsx
@@ -1,5 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { useGetProductByIdQuery } from "../store/slices/apiSlice";
+
 const SearchBarDrop = ({ items }) => {
-  console.log(items, "items koji primam");
+  // const productId = items._id;
+  // const {
+  //   data: product,
+  //   isLoading,
+  //   isError,
+  // } = useGetProductByIdQuery(productId);
+
+  // if (isLoading) return <p>Loading...</p>;
+  // if (isError) return <p>Error fetching product</p>;
+
+  // const navigateToProductHandler = () => {
+  //   const navigate = useNavigate();
+  //   navigate(`/products/${productId}`);
+  // };
+  // console.log("ovo treba da bude id ili product", product);
   return (
     <div className="searcBarDrop">
       <ul>
@@ -10,6 +27,7 @@ const SearchBarDrop = ({ items }) => {
                 src={product.imageUrl}
                 alt={"product image"}
                 className="searchBarDrop-img"
+                // onClick={() => navigateToProductHandler()}
               />
               <li key={product._id}>{product.title}</li>
             </div>
