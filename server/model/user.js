804e8c86393265a78e499b5996a10445e073b9d0
Sasa Nedic <svetasmak0@gmail.com>
2025-08-11 15:03:18 +0200

    Change structure of userSchema

--- server/model/user.js
+++ server/model/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import ProductsModel from "./products";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -6,6 +7,13 @@ const userSchema = new mongoose.Schema({
   country: { type: String, required: true },
   password: { type: String, required: true },
   confirmPassword: { type: String, required: true },
+  favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: ProductsModel }],
+  cart: [
+    {
+      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      quantity: { type: Number, default: 1 },
+    },
+  ],
 });
 
 const UserModel = mongoose.model("User", userSchema);
