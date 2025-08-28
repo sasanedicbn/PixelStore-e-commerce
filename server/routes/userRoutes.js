bd80d2c7058be3dfc61c8c6c1f2a09da809907c7
Sasa Nedic <svetasmak0@gmail.com>
2025-08-28 15:49:51 +0200

    Create useSendProdcutTOcartMutation

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -16,7 +16,7 @@ const routeUser = express.Router();
 routeUser.post("/", registerUser);
 routeUser.post("/login", loginUser);
 routeUser.post("/logout", protect, logoutUser);
-routeUser.post("/cart", protect, addProductToCart);
+routeUser.post("/cart/add", protect, addProductToCart);
 routeUser.get("/me", protect, getMe); //treba ubaciti protect
 routeUser.post("/contact", sendMessageUser);
 routeUser.get("/favourites", protect, getUserFavourites);
