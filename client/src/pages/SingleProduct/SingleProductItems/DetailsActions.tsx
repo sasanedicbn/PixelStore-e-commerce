5022e81ab2cc85350f6025a7125f3c2035cb3892
Sasa Nedic <svetasmak0@gmail.com>
2025-09-08 14:01:29 +0200

    Add some thing about update product

--- client/src/pages/SingleProduct/SingleProductItems/DetailsActions.tsx
+++ client/src/pages/SingleProduct/SingleProductItems/DetailsActions.tsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
 import { useUpdateCartMutation } from "../../../store/slices/apiSlice";
 import Button from "../../../UX/Button";
 import Icon from "../../../UX/Icons";
 
 const DetailsActions = ({ id }) => {
-  console.log(id, "iz details actions");
+  // console.log(id, "iz details actions");
+  const [quantity, setQuantity] = useState(1);
   const [updateCartItem] = useUpdateCartMutation();
   const updateCartItemHandler = async (productId, type) => {
     console.log(productId, type, "iz details actions");
     try {
-      await updateCartItem({ productId, type }).unwrap();
+      const userCart = await updateCartItem({ productId, type }).unwrap();
+      const updatedItem = userCart.cart.find((item) => item._id === id);
+      if (updatedItem) setQuantity(updatedItem.quantity);
+
+      console.log(userCart, "iz details actions datafromuser");
     } catch (error) {
       console.error("Failed to update cart item:", error);
     }
@@ -25,7 +31,13 @@ const DetailsActions = ({ id }) => {
           {" "}
           -{" "}
         </Button>
-        <input type="text" name="quantity" id="quantity" value={1} />
+        <input
+          type="text"
+          name="quantity"
+          id="quantity"
+          value={quantity}
+          readOnly
+        />
         <Button
           type="plus"
           onClick={() => {
