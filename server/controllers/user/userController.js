8cd9bd140c443332193d5617a86436dd34c2beea
Sasa Nedic <svetasmak0@gmail.com>
2025-09-21 22:02:35 +0200

    Finish with addReview function

--- server/controllers/user/userController.js
+++ server/controllers/user/userController.js
@@ -327,16 +327,24 @@ export const addReview = async (req, res) => {
   if (!productId) {
     return res.status(400).json({ message: "ProductId is required" });
   }
-  try{
+  try {
     const user = await UserModel.findById(req.user.id);
-    if(!user){
-      return res.status(404).json({message: "You should be logged in"})
+    if (!user) {
+      return res.status(404).json({ message: "You should be logged in" });
     }
     const product = await ProductsModel.findById(productId);
-    if(!product){
-      return res.status(404).json({message: "Product not found"})
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-   
-    
+    const newReview = {
+      review: comment,
+      user: user._id,
+      name: user.name,
+    };
+    product.reviews.push(newReview);
+    await product.save();
+    res.status(200).json({ success: true, message: "Review added", product });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Review is not added" });
   }
 };
