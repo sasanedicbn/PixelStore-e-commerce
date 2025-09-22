df6db1988876b447a58c69f34e20d22303dd935f
Sasa Nedic <svetasmak0@gmail.com>
2025-09-22 22:45:42 +0200

    Create changes in addReview function

--- server/controllers/user/productController.js
+++ server/controllers/user/productController.js
@@ -1,29 +1,35 @@
 export const addReview = async (req, res) => {
-  const { productId, comment } = req.body;
-  console.log("productId", productId);
-  console.log("comment", comment);
-  if (!comment) {
-    return res.status(400).json({ message: "You should add comment" });
+  const { review, rating } = req.body;
+  const { productId } = req.params;
+
+  if (!review) {
+    return res.status(400).json({ message: "You should add review text" });
   }
   if (!productId) {
     return res.status(400).json({ message: "ProductId is required" });
   }
+
   try {
     const user = await UserModel.findById(req.user.id);
     if (!user) {
       return res.status(404).json({ message: "You should be logged in" });
     }
+
     const product = await ProductsModel.findById(productId);
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
     }
+
     const newReview = {
-      review: comment,
+      review,
+      rating,
       user: user._id,
       name: user.name,
     };
+
     product.reviews.push(newReview);
     await product.save();
+
     res.status(200).json({ success: true, message: "Review added", product });
   } catch (error) {
     res.status(500).json({ success: false, message: "Review is not added" });
